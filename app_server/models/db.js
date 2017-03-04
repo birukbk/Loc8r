8d713072e4f56a7459f1c1076274cf4c40e6d318
Biruk G <birukbk@gmail.com>
2017-03-04 20:46:39 +0000

    Adds complete database connection

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var dbURI = 'mongodb://localhost/Loc8r';
 mongoose.connect(dbURI);
 
-// CONNECTION EVENTS
+/*CONNECTION EVENTS*/
 mongoose.connection.on('connected', function() {
     console.log('Mongoose connected to ' + dbURI);
 });
@@ -11,4 +11,38 @@ mongoose.connection.on('error', function(err) {
 });
 mongoose.connection.on('disconnected', function() {
     console.log('Mongoose disconnected');
+});
+/* CAPTURE APP TERMINATION / RESTART EVENTS
+To be called when process is restarted or terminated */
+gracefulShutdown = function(msg, callback) {
+    mongoose.connection.close(function() {
+        console.log('Mongoose disconnected through ' + msg);
+        callback();
+    });
+};
+/*For nodemon restarts*/
+process.once('SIGUSR2', function() {
+    gracefulShutdown('nodemon restart', function() {
+        process.kill(process.pid, 'SIGUSR2');
+    });
+});
+/*For app termination */
+process.on('SIGINT', function() {
+    gracefulShutdown('app termination', function() {
+        process.exit(0);
+    });
+});
+/*For Heroku app termination */
+
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
+});
+/*For Heroku app termination */
+
+process.on('SIGTERM', function() {
+    gracefulShutdown('Heroku app termination', function() {
+        process.exit(0);
+    });
 });
