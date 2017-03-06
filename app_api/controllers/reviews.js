f54c0761e10e73613d3e1a7bdf2ace4114cec760
Biruk G <birukbk@gmail.com>
2017-03-06 17:32:25 +0000

    Updates api_app with POST and GET locations

--- app_api/controllers/reviews.js
+++ app_api/controllers/reviews.js
@@ -9,3 +9,101 @@ var sendJsonResponse = function(res,status,content){
 // 	sendJsonResponse(res,200,{"status":"success"});
 // 	};
 // 	
+
+
+module.exports.reviewsReadOne = function(req, res) {
+  console.log("Getting single review");
+  if (req.params && req.params.locationid && req.params.reviewid) {
+    Loc
+      .findById(req.params.locationid)
+      .select('name reviews')
+      .exec(
+        function(err, location) {
+          console.log(location);
+          var response, review;
+          if (!location) {
+            sendJSONresponse(res, 404, {
+              "message": "locationid not found"
+            });
+            return;
+          } else if (err) {
+            sendJSONresponse(res, 400, err);
+            return;
+          }
+          if (location.reviews && location.reviews.length > 0) {
+            review = location.reviews.id(req.params.reviewid);
+            if (!review) {
+              sendJSONresponse(res, 404, {
+                "message": "reviewid not found"
+              });
+            } else {
+              response = {
+                location: {
+                  name: location.name,
+                  id: req.params.locationid
+                },
+                review: review
+              };
+              sendJSONresponse(res, 200, response);
+            }
+          } else {
+            sendJSONresponse(res, 404, {
+              "message": "No reviews found"
+            });
+          }
+        }
+    );
+  } else {
+    sendJSONresponse(res, 404, {
+      "message": "Not found, locationid and reviewid are both required"
+    });
+  }
+};
+
+
+
+/* POST a new review, providing a locationid */
+/* /api/locations/:locationid/reviews */
+module.exports.reviewsCreate = function(req, res) {
+  if (req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .select('reviews')
+      .exec(
+        function(err, location) {
+          if (err) {
+            sendJSONresponse(res, 400, err);
+          } else {
+            doAddReview(req, res, location);
+          }
+        }
+    );
+  } else {
+    sendJSONresponse(res, 404, {
+      "message": "Not found, locationid required"
+    });
+  }
+};
+
+
+var doAddReview = function(req, res, location) {
+  if (!location) {
+    sendJSONresponse(res, 404, "locationid not found");
+  } else {
+    location.reviews.push({
+      author: req.body.author,
+      rating: req.body.rating,
+      reviewText: req.body.reviewText
+    });
+    location.save(function(err, location) {
+      var thisReview;
+      if (err) {
+        sendJSONresponse(res, 400, err);
+      } else {
+        updateAverageRating(location._id);
+        thisReview = location.reviews[location.reviews.length - 1];
+        sendJSONresponse(res, 201, thisReview);
+      }
+    });
+  }
+};
