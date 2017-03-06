e14b146b56320fdae6728f321a87624ae7da2158
Biruk G <birukbk@gmail.com>
2017-03-06 13:12:22 +0000

    complete locationsReadOne function.

--- app_api/controllers/locations.js
+++ app_api/controllers/locations.js
@@ -1,10 +1,35 @@
 var mongoose = require('mongoose');
 var Loc = mongoose.model('Location');
-var sendJsonResponse = function(res, status, content){
+var sendJSONresponse = function(res, status, content){
 	res.status(status);
 	res.json(content);
 };
 
-module.exports.locationsReadOne = function(req, res){
-  sendJsonResponse(res, 200, {"status" : "success"});
+/* GET a location by the id */
+module.exports.locationsReadOne = function(req, res) {
+  console.log('Finding location details', req.params);
+  if (req.params && req.params.locationid) {
+    Loc
+      .findById(req.params.locationid)
+      .exec(function(err, location) {
+        if (!location) {
+          sendJSONresponse(res, 404, {
+            "message": "locationid not found"
+          });
+          return;
+        } else if (err) {
+          console.log(err);
+          sendJSONresponse(res, 404, err);
+          return;
+        }
+        console.log(location);
+        sendJSONresponse(res, 200, location);
+      });
+  } else {
+    console.log('No locationid specified');
+    sendJSONresponse(res, 404, {
+      "message": "No locationid in request"
+    });
+  }
 };
+
