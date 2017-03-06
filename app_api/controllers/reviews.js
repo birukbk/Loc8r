24939651677f0c0c8dd186e58e6d60003b676315
Biruk G <birukbk@gmail.com>
2017-03-06 12:01:40 +0000

    Adds definition of API routes.

---
+++ app_api/controllers/reviews.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Loc = mongoose.model('Location');
+var sendJsonResponse = function(res,status,content){
+	res.status(status);
+	res.json(content);
+};
+
+module.exports.reviewsReadOne = function(req,res){
+	sendJsonResponse(res,200,{"status":"success"});
+	};
